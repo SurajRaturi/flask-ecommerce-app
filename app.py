35560a5c7882abe6cf80a98229b3b126f8c2fed1
SurajRaturi <surajraturi.5122006@gmail.com>
2026-06-12 14:23:58 +0530

    added error message

--- app.py
+++ app.py
@@ -39,6 +39,10 @@ app.config["SQLALCHEMY_DATABASE_URI"]=os.getenv("DATABASE_URL","sqlite:///data.d
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
 app.config["JWT_SECRET_KEY"]=os.getenv("JWT_SECRET_KEY")
+
+if not app.config["JWT_SECRET_KEY"]:
+    raise ValueError("No JWT_SECRET_KEY set for Flask application")
+
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=5)
 
 app.config["UPLOAD_FOLDER"] = "static/uploads"
